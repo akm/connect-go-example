bf98a61046cc04095595f8cdb29846098d21c5a5
akm <akm2000@gmail.com>
2024-05-25 23:43:07 +0900

    :sparkles: Accept server URL and --grpc or --grpc-web option

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 
 	greetv1 "connect-go-example/gen/greet/v1"
 	"connect-go-example/gen/greet/v1/greetv1connect"
@@ -12,12 +13,24 @@ import (
 )
 
 func main() {
-	client := greetv1connect.NewGreetServiceClient(
-		http.DefaultClient,
-		"http://localhost:8080",
-		// connect.WithGRPC(),
-		// connect.WithGRPCWeb(),
-	)
+	var url string
+	if len(os.Args) < 2 {
+		url = "http://localhost:8080"
+	} else {
+		url = os.Args[1]
+	}
+
+	opts := []connect.ClientOption{}
+	if len(os.Args) > 2 {
+		switch os.Args[2] {
+		case "--grpc":
+			opts = append(opts, connect.WithGRPC())
+		case "--grpc-web":
+			opts = append(opts, connect.WithGRPCWeb())
+		}
+	}
+
+	client := greetv1connect.NewGreetServiceClient(http.DefaultClient, url, opts...)
 	res, err := client.Greet(
 		context.Background(),
 		connect.NewRequest(&greetv1.GreetRequest{Name: "Jane"}),
