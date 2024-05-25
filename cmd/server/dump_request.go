20ba09088099fc22bf1656bde82b925aa25e915c
akm <akm2000@gmail.com>
2024-05-26 00:06:33 +0900

    :sparkles: Use JSON for request dump

--- cmd/server/dump_request.go
+++ cmd/server/dump_request.go
@@ -1,21 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"net/http/httputil"
 )
+
+type dumpRequest struct {
+	Method string
+	URL    string
+	Proto  string
+	Header http.Header
+}
 
 func DumpRequestHandler(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Dump the request.
-		dump, err := httputil.DumpRequest(r, true)
+		dump := dumpRequest{
+			Method: r.Method,
+			URL:    r.URL.String(),
+			Proto:  r.Proto,
+			Header: r.Header,
+		}
+		b, err := json.Marshal(dump)
 		if err != nil {
 			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
 			return
 		}
-		log.Println(string(dump))
+		log.Println(string(b))
 
 		h.ServeHTTP(w, r)
 	})
